72467040348bf733a289ed3b9e031d57107e470a
Yesid Audor <yesidaudor123@gmail.com>
2024-10-16 22:21:19 -0500

    Agregué el holamundo.cpp

---
+++ holamundo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+    /*
+    int x; //Declaracion
+    x = 5; //Asignacion
+    */
+    //Enteros
+    int x = 5; 
+    int y = 6;
+    int suma = x + y;
+    int edad = 24;
+    int año = 2024; 
+    int dias = 24;
+
+    //Double o Flotante
+
+    double precio = 10.99;
+    double promedio = 2.5;
+    double temperatura = 25.1;
+
+    //char
+    char clasificacion = 'A';
+    char inicial = 'A';
+
+
+    
+    float precio2 = 10.99;
+    
+    //boleanos ('bool)
+    bool estudiante = false;
+    bool encendido = true;
+    bool venta = true;
+    
+
+    //Sttring
+    std::string nombre = "Yesid";
+
+    std::cout<<"hola "<<nombre;
+
+    return 0;
+}
